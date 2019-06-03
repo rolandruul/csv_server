c02ac115aa64c5c5a5bb8dcf108691674ba0ff87
Roland Ruul <rolts@live.com>
2019-06-03 10:06:38 +0300

    cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ let results = [];
 //app.use(cors());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 const storage = multer.diskStorage({
